3433ba367573b1bbbea7be82719681c1d947f89a
Dmitry Grebenin <dminin2001@gmail.com>
2019-11-07 19:40:32 +0200

    major changes, session creation done(more or less), skeleton of the question page

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+let sessionID;
+let numOfQuestions;
+
 async function getHuntList() {
     fetch("https://codecyprus.org/th/api/list")
         .then(response => response.json())
@@ -8,8 +11,7 @@ async function getHuntList() {
                 //Create and append hunt name
                 let nameElement = document.createElement("li");
                 huntList.appendChild(nameElement);
-                nameElement.innerHTML = ("<a href='https://codecyprus.org/th/api/start?player=Homer&app=simpsons-app&treasure-hunt-id=" + 
-                treasureHunt.uuid + "' alt='Treasure hunt " + treasureHunt.name + "'>" + treasureHunt.name + "</a>")
+                nameElement.innerHTML = ("<a href='#' onclick='startSession(\"" + treasureHunt.uuid + "\")' alt='Treasure hunt " + treasureHunt.name + "'>" + treasureHunt.name + "</a>")
 
                 //Create and append sublist for hunt info
                 let subList = document.createElement("ul");
@@ -28,6 +30,38 @@ function timestampFromEpoch(epochTime) {
         ", " + dateObj.getHours() + ":" + dateObj.getMinutes()
         .toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping:false}) + ":" + 
         dateObj.getSeconds().toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping:false}));
+}
+
+async function startSession(uuid) {
+    fetch("https://codecyprus.org/th/api/start?player=" + "boi2" + "&app=dac-app&treasure-hunt-id=" + uuid)
+        .then(response => response.json()).then(responseJSON => {
+            if (responseJSON.status !== "ERROR") {
+                sessionID = responseJSON.session;
+                numOfQuestions = responseJSON.numOfQuestions;
+                prepareQuestionPage(sessionID);
+            } else {
+                let errorString = "";
+                for (let errorMessage of responseJSON.errorMessages) {
+                    errorString += errorMessage;
+                }
+                alert(errorString);
+            }
+
+        });
+}
+
+function prepareQuestionPage(session) {
+    let body = document.getElementsByTagName("body")[0];
+    body.innerHTML = "";
+    let questionName = document.createElement("h1");
+    let answerList = document.createElement("ol");
+    let answerField = document.createElement("div");
+    questionName.innerText = "Question name";
+    answerList.innerHTML = "<li>answer1</li><li>answer2</li>";
+    answerField.innerHTML = "<button>Yeet</button><button>Yeet2</button>"
+    body.appendChild(questionName);
+    body.appendChild(answerList);
+    body.appendChild(answerField);
 }
 
 getHuntList();
