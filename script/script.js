574b0843963f1a345ce9983b27b09a4752938321
Dmitry Grebenin <dminin2001@gmail.com>
2019-11-14 15:06:06 +0200

    start work on prepareQuiz

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 const API = "https://codecyprus.org/th/api"
+let sessionID = "";
 
 async function getHuntList() {
     fetch(API + "/list")
@@ -37,18 +38,25 @@ function timestampFromEpoch(epochTime) {
 
 function startSession(uuid) {
     let username = document.getElementById('usernameBox').value;
+    document.getElementById('errorBox').classList.remove('done', 'error', 'disable');
+    document.getElementById('errorBox').classList.add('loading');
+    document.getElementById('errorBox').innerText = "Loading...";
     fetch(API + "/start?player=" + username + "&app=dac-name&treasure-hunt-id=" + uuid)
         .then(response => response.json())
         .then(jsonResponse => {
-            console.log(jsonResponse);
             if (jsonResponse.status === "ERROR") {
-                document.getElementById('errorBox').style.display = "inline";
+                document.getElementById('errorBox').classList.remove('done', 'loading');
+                document.getElementById('errorBox').classList.add('error');
                 document.getElementById('errorBox').innerText = "";
                 for (let errorMessage of jsonResponse.errorMessages) {
                     document.getElementById('errorBox').innerText += errorMessage;
                 }
             } else {
-                prepareQuiz(jsonResponse.session);
+                document.getElementById('errorBox').classList.remove('loading', 'error');
+                document.getElementById('errorBox').classList.add('done');
+                document.getElementById('errorBox').innerText = "Session created!"
+                sessionID = jsonResponse.session;
+                prepareQuiz();
             }
         });
 }
@@ -61,23 +69,36 @@ function enterUsername(uuid, targetID) {
         document.getElementById('inputBox').remove();
     }
     usernameInput = document.createElement("div");
-        usernameInput.id = "inputBox";
-        usernameInput.style.display = "inline-block";
-        usernameInput.style.marginLeft = "10px";
-        usernameInput.innerHTML = "<span>Username: </span>" +
-                                "<input id='usernameBox' type='text'></input>" + 
-                                "<button id='submitButton' onclick='startSession(\""+ uuid + "\")'>OK</button>" +
-                                "<span id='errorBox' style='padding:2px; margin-left: 10px;background-color: red; color: white; display:none'></span>";
-        target.appendChild(usernameInput);
-        document.getElementById('usernameBox').addEventListener("keyup", function(event) {
-            if (event.keyCode == 13) {
-                document.getElementById('submitButton').click();
-            }
+    usernameInput.id = "inputBox";
+    usernameInput.style.display = "inline-block";
+    usernameInput.style.marginLeft = "10px";
+    usernameInput.innerHTML = "<span>Username: </span>" +
+                            "<input id='usernameBox' type='text'></input>" + 
+                            "<button id='submitButton' onclick='startSession(\""+ uuid + "\")'>OK</button>" +
+                            "<span id='errorBox' class='disable' style='padding:2px; margin-left: 10px'></span>";
+    target.appendChild(usernameInput);
+    document.getElementById('usernameBox').addEventListener("keyup", function(event) {
+        if (event.keyCode == 13) {
+            document.getElementById('submitButton').click();
+        }
         });
 }
 
-function prepareQuiz(sessionID) {
+function prepareQuiz() {
     console.log(sessionID);
+    fetch(API + "/question?session=" + sessionID)
+        .then(response => response.json())
+        .then(jsonResponse => {
+            if (jsonResponse.status === "ERROR") {
+                let errorMessageList = "";
+                for (let errorMessage of jsonResponse.errorMessages) {
+                    errorMessageList += errorMessage + "\n";
+                }
+                alert(errorMessageList);
+            } else {
+                document.getElementsByTagName('body')[0].innerHTML = "WIP";
+            }
+        });
 }
 
 getHuntList();
