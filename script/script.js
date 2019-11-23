3ba0e7d91f945bea336464043abf14a645801060
Alexandros Christou <achristou4@uclan.ac.uk>
2019-11-23 13:47:43 +0200

    leaderboard->print first 10 players, sorting attempt(sort array and print 10 higher)

--- script/script.js
+++ script/script.js
@@ -6,16 +6,7 @@ function getHuntList() {
     fetch(API + "/list")
         .then(response => response.json())
         .then(responseJSON => {
-            let huntList = document.getElementById("huntList");
-            if (getCookie('gamePlaying') === 'true') {
-                let cookieSessionID = getCookie('sessionID');
-                let username = getCookie('username');
-                let nameElement = document.createElement('li');
-                nameElement.id = 'savedHuntName';
-                nameElement.innerHTML = '<a style="font-weight: bold" href="javascript:startSession(\'' + cookieSessionID + "\', undefined, true, \'" + username + '\')">Continue previous game</a>'
-                huntList.appendChild(nameElement);
-            }
-
+            let i = 0;
             for (let treasureHunt of responseJSON.treasureHunts) {
                 let huntList = document.getElementById("huntList");
                 let startDateObj = new Date(treasureHunt.startsOn);
@@ -29,13 +20,12 @@ function getHuntList() {
                     hour12: false
                 };
                 let endDateObj = new Date(treasureHunt.endsOn);
-
                 //Create and append hunt name
                 let nameElement = document.createElement("li");
                 nameElement.id = "thName" + i;
                 huntList.appendChild(nameElement);
                 nameElement.innerHTML = ("<a style='font-weight: bold;' href='javascript:enterUsername(\"" +
-                    treasureHunt.uuid + "\", \"" + nameElement.id + "\", \"" + endDateObj.toUTCString() + "\")'>" + treasureHunt.name + "</a>");
+                    treasureHunt.uuid + "\", \"" + nameElement.id + "\")'>" + treasureHunt.name + "</a>");
 
                 //Create and append sublist for hunt info
                 let subList  = document.createElement("ul");
@@ -52,7 +42,7 @@ function getHuntList() {
 }
 
 // noinspection JSUnusedGlobalSymbols
-function startSession(uuid, expiryDate) {
+function startSession(uuid) {
     let username = document.getElementById('usernameBox').value;
     document.getElementById('errorBox').classList.remove('done', 'error', 'disable');
     document.getElementById('errorBox').classList.add('loading');
@@ -73,8 +63,6 @@ function startSession(uuid, expiryDate) {
                 document.getElementById('errorBox').classList.add('done');
                 document.getElementById('errorBox').innerText = "Session created!";
                 sessionID = jsonResponse.session;
-                document.cookie = 'gamePlaying=true; expires=' + expiryDate;
-                document.cookie = 'sessionID=' + sessionID + 'expires=' + expiryDate;
                 getQuestion();
             }
         });
@@ -82,7 +70,7 @@ function startSession(uuid, expiryDate) {
 }
 
 // noinspection JSUnusedGlobalSymbols
-function enterUsername(uuid, targetID, huntEndDate) {
+function enterUsername(uuid, targetID) {
     let target = document.getElementById(targetID);
     if (document.getElementById('inputBox') !== null) {
         document.getElementById('inputBox').remove();
@@ -91,9 +79,9 @@ function enterUsername(uuid, targetID, huntEndDate) {
     usernameInput.id = "inputBox";
     usernameInput.style.display = "inline-block";
     usernameInput.style.marginLeft = "10px";
-    usernameInput.innerHTML =   "<form action='javascript:startSession(\"" + uuid + "\", \"" + huntEndDate + "\")'>" +
+    usernameInput.innerHTML =   "<form action='javascript:startSession(\"" + uuid + "\")'>" +
                                         "<input id='usernameBox' type='text' placeholder='Enter your username'>" +
-                                        "<input type='submit' value='Submit' style='' class='submitButton'>" +
+                                        "<input type='submit' style='' class='submitButton'>" +
                                         "<span id='errorBox' class='disable' style='padding:2px; margin-left: 10px'></span>" +
                                 "</form>";
     target.appendChild(usernameInput);
@@ -214,7 +202,7 @@ function getQuestion() {
                             break;
                         case "TEXT":
                             let textForm = document.createElement('form');
-                            textForm.action = 'javascript:sendAnswer(document.getElementById("textBox").value)';
+                            textForm.action = 'javascript:sendAnswer(document.getElementById("textBox").value)'
 
                             let textBox = document.createElement('input');
                             textBox.type = 'text';
@@ -272,7 +260,6 @@ function sendAnswer(answer) {
                 document.getElementById('outputMSG').classList.remove('disable', 'done');
                 document.getElementById('outputMSG').classList.add('error');
                 document.getElementById('outputMSG').innerText = responseJSON.errorMessages[0];
-                endSession();
             }
         });
 }
@@ -300,8 +287,6 @@ function skipQuestion() {
 }
 
 function endSession() {
-    document.cookie = 'gamePlaying=; expires=Thu 01 Jan 1970';
-    document.cookie = 'sessionID=; expires=Thu 01 Jan 1970';
     document.body.innerHTML = "end";
     console.log('end');
     getLeaderboard()
@@ -328,60 +313,51 @@ function getLocation() {
     }
 }
 
-function getCookie(cookieName) {
-    let name = cookieName + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
 function getLeaderboard() {
     fetch("https://codecyprus.org/th/api/leaderboard?session=" + sessionID)
         .then(response => response.json())
         .then(responseJSON => {
 
-            //  console.log("i am in");
-            //  let tableScore = document.createElement("TABLE");
-            // document.body.innerHTML="hre";
+           //  console.log("i am in");
+           //  let tableScore = document.createElement("TABLE");
+           // document.body.innerHTML="hre";
 
             let score = responseJSON.leaderboard;
             score.id= "score";
-            let scoreStatus = responseJSON.status;
-            console.log(scoreStatus);
-            let numOFplayes = responseJSON.numOfPlayers;
-            console.log(numOFplayes);
 
-            // let hasPrize = responseJSON.hasPrice;
-            // console.log(hasPrize);
-            let limit = responseJSON.limit;
-            limit=10;
-            console.log(limit);
-
+            let object;
             // document.body.innerText = score.
             console.log(score);
             document.createElement("table");
 
-            //  let jasonScore = JSON.stringify(score);
-            // document.body.innerText = jasonScore;
+           let jasonScore = JSON.stringify(score);
+          document.body.innerText = jasonScore;
 
-            for  (let scoreObject of score){
-                console.log(scoreObject);
-                let jasonScore = JSON.stringify(score);
-                document.body.innerText = jasonScore;
 
-            }
-            // let playrName = jasonScore.player;
-            // console.log(playrName);
+        //   console.log(array);
+
+            // score.sort();
+            // score.reverse();
+            // object = document.getElementById("score");
+            // -->DONT DELETE YET
+
+           for (let i=0; i<10; i++){
+               console.log(score[i]);
+               document.body.innerText = jasonScore[i];
+           }
+
+
+
 
         });
 }
+
+// function getLeaderboard() {
+//
+//     fetch("https://codecyprus.org/th/api/leaderboard?session=" + sessionID, { method: "GET" })
+//         .then(response => response.json())
+//         .then(json => handleLeaderboard(json));
+// }
+
 
 getHuntList();
