f081cca2bb10c6e1864b54627ad6cc9eaf8568f7
CMouskovia <56922074+CMouskovia@users.noreply.github.com>
2019-11-29 14:45:17 +0200

    Testing for switch cameras button

--- script/script.js
+++ script/script.js
@@ -142,6 +142,16 @@ function getQuestion() {
                         document.getElementById("qrCode").name="qrCode";
                         qrCode.addEventListener('click', QRCode);
 
+                        let switchC = document.createElement("switchC");
+                        switchC.id = "switchC";
+                        switchC.classList.add('button');
+                        switchC.innerText="Switch the camera";
+                        document.body.appendChild(switchC);
+                        document.getElementById("switchC").value="switchC";
+                        document.getElementById("switchC").name="switchC";
+                        switchC.addEventListener('click', changeCam);
+
+
                     } else {
                         let errorSkip = document.createElement("p");
                         errorSkip.innerText = "Cannot skip. This questions is defined as one that cannot be skipped.";
@@ -421,7 +431,62 @@ function getLeaderboard() {
 
         })
 }
+function changeCam(){
+    let getVideo = document.createElement("video");
+    getVideo.id = 'preview';
+    document.body.appendChild(getVideo);
 
+    let opts = {
+        // Whether to scan continuously for QR codes. If false, use scanner.scan() to manually scan.
+        // If true, the scanner emits the "scan" event when a QR code is scanned. Default true.
+        continuous: true,
+
+        // The HTML element to use for the camera's video preview. Must be a <video> element.
+        // When the camera is active, this element will have the "active" CSS class, otherwise,
+        // it will have the "inactive" class. By default, an invisible element will be created to
+        // host the video.
+        video: document.getElementById('preview'),
+
+        // Whether to horizontally mirror the video preview. This is helpful when trying to
+        // scan a QR code with a user-facing camera. Default true.
+        mirror: false,
+
+        // Whether to include the scanned image data as part of the scan result. See the "scan" event
+        // for image format details. Default false.
+        captureImage: false,
+
+        // Only applies to continuous mode. Whether to actively scan when the tab is not active.
+        // When false, this reduces CPU usage when the tab is not active. Default true.
+        backgroundScan: true,
+
+        // Only applies to continuous mode. The period, in milliseconds, before the same QR code
+        // will be recognized in succession. Default 5000 (5 seconds).
+        refractoryPeriod: 5000,
+
+        // Only applies to continuous mode. The period, in rendered frames, between scans. A lower scan period
+        // increases CPU usage but makes scan response faster. Default 1 (i.e. analyze every frame).
+        scanPeriod: 1
+    };
+
+    let scanner = new Instascan.Scanner(opts);
+
+
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            console.log(cameras);
+            scanner.start(cameras[0]);
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
+
+    scanner.addListener('scan', function (content) {
+        console.log(content);
+    });
+
+}
 
 
 
